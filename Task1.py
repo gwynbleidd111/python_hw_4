28658f5f0dd925e1db4b935c808aec8b3a1e5076
gwynbleidd111 <94562956+gwynbleidd111@users.noreply.github.com>
2022-10-24 16:25:17 +0300

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,31 @@
+Задача №1___________________________________________________________
+
+def get_number():
+    number = int(input("Введите число: "))
+    return number
+
+def get_multipliers(number):
+    i = 2
+    multipliers = []
+    while i * i <= number:
+        while number % i == 0:
+            multipliers.append(i)
+            number = number // i
+        i = i + 1
+    if number > 1:
+        multipliers.append(number)
+    return multipliers
+
+def transformation(list):
+    new_list = ', '.join([str(x) for x in list])
+    return new_list
+
+def write(list):
+    data = open("text.txt", "w")
+    data.writelines(list)
+    data.close()
+
+a = get_number()
+b = get_multipliers(a)
+c = transformation(b)
+write(c)
